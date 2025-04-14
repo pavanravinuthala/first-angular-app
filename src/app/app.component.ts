9bd1477dd60c0edd8d58cc7ae010600ccf2542dc
Pavan Ravinuthala <pavanravinuthala@users.noreply.github.com>
2025-04-14 07:06:59 +0530

    32

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { DUMMY_USERS } from './dummy.users';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
+  users = signal(DUMMY_USERS);
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, Input, input } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy.users';
-
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,13 +9,22 @@ import { DUMMY_USERS } from '../../dummy.users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  userdata = DUMMY_USERS[1];
+  // @Input({ required: true }) userimage!: string;
+  // @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  userimage = input.required<string>();
+  name = input.required<string>();
+  id = input.required<string>();
 
+  //userdata = DUMMY_USERS[randomIndex];
+  // userdata = signal(DUMMY_USERS[randomIndex]);
+  // userimages = computed(() => '/assets/users/' + this.userdata().avatar);
   get userimagepath() {
-    return '/assets/users/' + this.userdata.avatar;
+    return '/assets/users/' + this.userimage();
   }
-
-  userbuttonclicked() {
-    console.log('Button Clicked');
-  }
+  // userbuttonclicked() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.userdata.set(DUMMY_USERS[randomIndex]);
+  //   //this.userdata = DUMMY_USERS[randomIndex];
+  // }
 }
